704c90b2976d10cf6c723a333978d353c0399355
toto9602 <toto9602@naver.com>
2022-04-29 13:32:47 +0900

    [feat]: polygon  초안

---
+++ backend/models/area.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/db');
+const {DataTypes} = require('sequelize');
+
+const Area = sequelize.define("Area", {
+    pk: {
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        primaryKey:true,
+        autoIncrement:true,
+    },
+    name: {
+        type:DataTypes.STRING(20),
+        allowNull:false,
+    },
+    polygon: {
+        type:DataTypes.GEOMETRY('POLYGON'),
+        allowNull:false
+    }
+}, {
+    freezeTableName:true,
+    timestamps:false
+}
+);
+
+module.exports = Place;
