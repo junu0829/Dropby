78ee3449ebc5c2cc99891d8dfa53c3262aa38230
toto9602 <toto9602@naver.com>
2022-05-14 20:41:30 +0900

    [fix]: multer js 추가

--- backend/utils/multer.js
+++ backend/utils/multer.js
@@ -2,7 +2,6 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const aws = require('aws-sdk');
 aws.config.loadFromPath(__dirname + '/../config/s3.json');
-console.log(__dirname + '/../config/s3.json')
 const s3 = new aws.S3();
 const upload = multer({
     storage: multerS3({
