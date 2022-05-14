5c6b887f2400ab665c24988cf37f2204ec455d06
toto9602 <toto9602@naver.com>
2022-05-14 19:53:42 +0900

    [feat]: 로그인 요구 있는 게 나을 듯

--- backend/routes/dropRouter.js
+++ backend/routes/dropRouter.js
@@ -3,10 +3,9 @@ const passport = require('passport');
 const controller = require('../controllers/dropController');
 const jwtpassportAuth = passport.authenticate('jwtAccess', {session:false});
 
-//jwtpassportAuth 임시로 삭제함. 다시 추가 필요
-router.post('/', controller.newDrop);
-router.get('/', controller.getDrops);
-router.put('/:dropPk', controller.updateDrop);
-router.delete('/:dropPk', controller.deleteDrop);
+router.post('/', jwtpassportAuth, controller.newDrop);
+router.get('/', jwtpassportAuth, controller.getDrops);
+router.put('/:dropPk', jwtpassportAuth, controller.updateDrop);
+router.delete('/:dropPk', jwtpassportAuth, controller.deleteDrop);
 
 module.exports = router;
