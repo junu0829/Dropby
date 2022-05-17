7ce1705c6740df33c34d6d5511e8d80cf46ee903
toto9602 <toto9602@naver.com>
2022-05-17 13:35:47 +0900

    [feat]: services 작성 및 구역 내 드롭 조회 점검 완료

--- backend/services/placeServices.js
+++ backend/services/placeServices.js
@@ -46,15 +46,28 @@ exports.getAreaDrops = async (areaPk) => {
         }
     });
     const areaName = area.name;
-    const dropsData = await Drop.findAll({
+
+    const AreaPlaces = await Place.findAll({
         where:{
             areaPk
         }
     });
-
+    
+    let allDrops = []
+    for (let place of AreaPlaces) {
+        let placePk = place.dataValues.pk;
+        let drops = await Drop.findAll({
+            where: {
+                placePk
+            }
+        })
+        if (drops.length > 0) {
+            allDrops.push(...drops);
+        }
+    }
     return {
         'areaName':areaName,
         'areaPk':areaPk,
-        'dropsData':dropsData
+        'drops':allDrops
     }
 }
