1383c2f6475bbff63813d366ce1a31a6d6ab0179
Dozi <ytooom019@naver.com>
2022-07-11 14:56:45 +0900

    [FE]유민 postDrop 버그 픽스

--- frontend/src/services/drops/postDrop.js
+++ frontend/src/services/drops/postDrop.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { user } from "../user.js";
 
 export const postDrop = async (area, placePk, frm) => {
-  console.log(frm);
   user.getItemFromAsync();
   // await axios(`http://${LOCAL_HOST}:3000/${area}/${placePk}/drops`, {
   //   method: "POST",
@@ -21,12 +20,13 @@ export const postDrop = async (area, placePk, frm) => {
   fetch(`http://${LOCAL_HOST}:3000/${area}/${placePk}/drops`, {
     method: "POST",
     headers: {
-      Accept: "application/json",
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwayI6MSwiZW1haWwiOiJ0ZXN0MkB0ZXN0LmNvbSIsImlhdCI6MTY1Njk0MjA3MiwiZXhwIjoxNjU2OTQ1NjcyfQ.BXbTHVYjAASZCJDFttTL8EilgYJJgWRT3Wrx41DIPZc",
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwayI6MSwiZW1haWwiOiJ0ZXN0MkB0ZXN0LmNvbSIsImlhdCI6MTY1NzUxODYzOSwiZXhwIjoxNjYwMTEwNjM5fQ.X2q6YnNQGgq85xdCAP7zodcGMG26h3uT2v3oglmAadM",
     },
     body: frm,
   })
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((response) =>
+      console.log(`${response.data.content} 내용으로 ${response.msg}!`)
+    );
 };
