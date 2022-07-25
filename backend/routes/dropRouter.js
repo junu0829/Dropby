e2e1c99c56a661bbc47e5e94d2ba2f6c7a341a67
toto9602 <toto9602@naver.com>
2022-07-25 22:47:01 +0900

    feat : dropRouter에 commentRouter 연결

--- backend/routes/dropRouter.js
+++ backend/routes/dropRouter.js
@@ -2,6 +2,7 @@ const router = require("express").Router({ mergeParams: true });
 const passport = require("passport");
 const controller = require("../controllers/dropController");
 const jwtpassportAuth = passport.authenticate("jwtAccess", { session: false });
+const comment = require('../routes/commentRouter');
 const upload = require('../utils/multer');
 
 router.post("/", jwtpassportAuth, upload.array('image', 5), controller.newDrop);
@@ -11,5 +12,7 @@ router.get('/my', jwtpassportAuth, controller.getMyDrops);
 router.put("/:dropPk", jwtpassportAuth, upload.array('image', 5), controller.updateDrop);
 router.delete("/:dropPk", jwtpassportAuth, controller.deleteDrop);
 router.get('/:dropPk', jwtpassportAuth, controller.getDrop);
+
+router.use('/:dropPk', jwtpassportAuth, comment);
 
 module.exports = router;
